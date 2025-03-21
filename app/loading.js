231f45688652625271304e57162fb2b1737aa0b7
Sadan Mian <sadanmiyan007@gmail.com>
2025-03-22 02:30:44 +0530

    loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,7 +1,8 @@
-import React from "react";
-
-function loading() {
-  return <div>loading</div>;
+// app/conferences/loading.js
+export default function Loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+    </div>
+  );
 }
-
-export default loading;
